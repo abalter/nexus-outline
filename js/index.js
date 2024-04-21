1cf006c9bbd18c3c4207e4b572c8753f5109d444
Ariel Balter <ariel@arielbalter.com>
2024-04-21 18:16:09 +0000

    new front page

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const text_processor = MML.getInstance();
 
-fetch('articles/article.mml')
+fetch('articles/front_page.mml')
   .then(_ => _.text())
   .then(_ => text_processor.processText(_))
   .then(_ => fillOutline(_))
-  // .then(_ => console.log(_))
-  // .then(_ => console.log(_.documentElement.innerHTML))
-  // .then(_ => fillOutline(text_processor.processText(_));
+// .then(_ => console.log(_))
+// .then(_ => console.log(_.documentElement.innerHTML))
+// .then(_ => fillOutline(text_processor.processText(_));
 
